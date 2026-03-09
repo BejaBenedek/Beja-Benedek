37012d46e511f2f922b7e74179140604952a54c1
BoroReka <juraszik.reka2009@tanulo.boronkay.hu>
2026-03-09 13:02:21 +0100

    first commit

---
+++ eredmenyek = [].py
@@ -0,0 +1,18 @@
+primek = []
+
+i = 2
+hatar = 12
+while i < hatar:
+       prim = False  
+    i += i
+
+prim = True
+x = i
+ if x == 2
+    prim = True
+elif X
+
+
+print("ennyi prímet találtam: ",len(primek))
+print("primek")
+print("a legnagyobb prím", primek[len(primek)])
